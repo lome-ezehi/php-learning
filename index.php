75b3a24178075340d3289a42eff27c18ca6dfa5a
Lomeeee <lomeezehi@gmail.com>
2024-09-02 12:45:07 +0100

    added materialize file

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $your_name = "Lome";
+    echo $your_name; 
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- <link rel="shortcut icon" href="img/rcon.png" type="image/x-icon"> -->
+        <link rel="stylesheet" href="css/materialize.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <title>Recipe.com</title>
+    </head>
+    <body>
+        <h1 class = "red-text">Hello my name is <?php echo $your_name; ?> and i'm learning php</h1>
+        <script src="js/jqueryv3.4.1.js"></script>
+        <script src="js/materialize.js"></script>
+        <script>
+            $(document).ready(function(){
+
+            })
+        </script>
+    </body>
+</html>
