397d03a7cedc94752ca51645f28e497ec22878d3
Lomeeee <lomeezehi@gmail.com>
2024-09-03 20:27:17 +0100

    classwork on the foreach loop

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ I started learning php today.
 
 $variable - variable
 echo $variable; - call variable
-pi() - pi value
+pi() - pi value (number function)
 round() - round to 2 decimal
 strlen(variable) - string length
 rand(1, 10) - random values between 1 - 10
--- students.php
+++ students.php
@@ -1,16 +1,27 @@
 <?php
     //multidimensional array foreach loop
     $students = [
-        ["name" => "Emeka Charles", "matric_no" => "VUG/SEN/23/8868"],
-        ["name" => "David King", "matric_no" => "VUG/SEN/23/8176"],
-        ["name" => "Ezehi Lome", "matric_no" => "VUG/SEN/23/8365"],
-        ["name" => "Busayomi Abass", "matric_no" => "VUG/SEN/23/9138"]
+        ["name" => "Emeka Charles", "matric_no" => "VUG/SEN/23/8868", "fees_status" => "paid"],
+        ["name" => "David King", "matric_no" => "VUG/SEN/23/8176", "fees_status_u" => "unpaid"],
+        ["name" => "Ezehi Lome", "matric_no" => "VUG/SEN/23/8365", "fees_status" => "paid"],
+        ["name" => "Busayomi Abass", "matric_no" => "VUG/SEN/23/9138", "fees_status_u" => "unpaid"]
     ];
     // foreach ($students as $student) {
     //     echo $student["name"] . "<br>";
     // }
 
+    //functons
+    function hello_world($user = "guest") {
+        echo "Good Afternoon, $user";
+    }
+    hello_world("Lome the Boss");
+    hello_world();
 
+    function volume_of_sphere($radius = 0){
+        $answer = (4/3) * pi() * $radius**3;
+        echo $answer;
+    }
+    volume_of_sphere(5);
 
 
 
@@ -22,18 +33,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/materialize.css">
     <title>LOME-PHP</title>
+    <style>
+
+    </style>
 </head>
 <body>
     <div class="container">
-        <h1 class = "center-align green">Students</h1>
+        <h1 class = "center-align green ">Students</h1>
         <div class="row">
-            <?php foreach ($students as $student) { ?>
+            <?php foreach ($students as $student) {
+                if ($student["fees_status"] == true) {
+                    $color = "green";
+                }else {
+                    $color = "red";
+                }
+            
+            ?>
                 <div class="col l3 s12">
-                    <div class="card">
+                    <div class="card" style= "background-color: <?php echo $color; ?>">
                         <div class="card-image"></div>
                         <div class="card-content">
-                            <span class="card-title green-text"><?php echo $student["name"]; ?> </span>
-                            <span class = "green-text"> <?php echo $student["matric_no"]; ?> </span>
+                            <span class="card-title white-text"> <?php echo $student["name"];?> </span>
+                            <span class = "white-text"> <?php echo $student["matric_no"]; ?> </span>
+                            <span class = "white-text"> <?php echo $student["fees_status"]; ?> </span>
+                            <span class = "white-text"> <?php echo $student["fees_status_u"]; ?> </span>
                         </div>
                     </div>
                 </div>
